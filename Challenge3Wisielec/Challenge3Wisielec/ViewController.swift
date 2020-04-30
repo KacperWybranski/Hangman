58f12df253c48aa49af3020773a83b5bf27e29f4
KacperWybranski <63157451+KacperWybranski@users.noreply.github.com>
2020-04-30 17:21:46 +0200

    Uploading starting files of my Project
    Is it right uploading things like that?

---
+++ Challenge3Wisielec/Challenge3Wisielec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge3Wisielec
+//
+//  Created by test on 30/04/2020.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
